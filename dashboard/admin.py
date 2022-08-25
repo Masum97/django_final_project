d1352468146115258976078d523375fe373df755
Masum97 <96322959+Masum97@users.noreply.github.com>
2022-08-25 18:27:16 +0600

    order model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from . models import Product
+from .models import Product, Order
+
+
+from django.contrib.auth.models import Group
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 CATEGORY = (
@@ -15,3 +17,12 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}- {self.quantity}'
+
+
+class Order(models.Model):
+    name = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.customer}-{self.name}'
